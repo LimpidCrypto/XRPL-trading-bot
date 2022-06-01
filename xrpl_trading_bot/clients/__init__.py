8a102ace982eac5f9c0f7146f6995e8084c85b0a
LimpidCrypto <97235361+LimpidCrypto@users.noreply.github.com>
2022-06-01 22:52:27 +0200

    add get_gateway_fees

--- xrpl_trading_bot/clients/__init__.py
+++ xrpl_trading_bot/clients/__init__.py
@@ -1,11 +1,13 @@
 from xrpl_trading_bot.clients.main import xrp_request_async
 from xrpl_trading_bot.clients.methods import (
+    get_gateway_fees,
     subscribe_to_account_balances,
     subscribe_to_order_books,
 )
 from xrpl_trading_bot.clients.websocket_uri import FullHistoryNodes, NonFullHistoryNodes
 
 __all__ = [
+    "get_gateway_fees",
     "subscribe_to_account_balances",
     "subscribe_to_order_books",
     "xrp_request_async",
--- xrpl_trading_bot/globals/__init__.py
+++ xrpl_trading_bot/globals/__init__.py
@@ -1,7 +1,8 @@
 from xrpl_trading_bot.globals.constants import WALLET
-from xrpl_trading_bot.globals.variables import all_order_books
+from xrpl_trading_bot.globals.variables import all_order_books, gateway_fees
 
 __all__ = [
     "all_order_books",
+    "gateway_fees",
     "WALLET",
 ]
